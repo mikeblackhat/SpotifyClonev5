b80490283b470aaea468929982e4b54a2e0756c1
Mikeblackhat <31170719+mikeblackhat@users.noreply.github.com>
2025-05-19 13:11:06 -0500

    version-0.0.1

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+const Button = () => {
+    return (
+        <button
+            className="
+                bg-white
+                rounded-full
+                py-2
+                px-4
+                font-semibold
+                text-sm
+                text-black
+                hover:bg-gray-200
+                transition
+            "
+        >
+            Log In
+        </button>
+    );
+}
+export default Button;
